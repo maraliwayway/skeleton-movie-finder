33f5dde9b112c478daf3e86158ecbea19780cc57
Mara Liwayway David <115133366+maraliwayway@users.noreply.github.com>
2025-03-13 10:32:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// JS Step 1: API configuration
+
+
+// JS Step 2: DOM Elements
+
+
+// JS Step 3: State
+
+
+// JS Step 4: Event Listeners
+
+// 4.1: Click searchButton or press Enter key in searchInput to trigger searchMovies()
+
+// 4.2: Click searchViewBtn to activate the search view and hide the favorites
+
+
+// 4.3: Click favoritesViewBtn to activate the favorites view and hide the search view
+
+
+// JS Step 5: Define core functions
+
+// 5.1: searchMovies()
+
+
+// 5.2: displayMovies()
+
+
+// IF TIME: 5.3: toggleFavorite()
+
+
+
+
+// IF TIME: 5.4: displayFavorites()
